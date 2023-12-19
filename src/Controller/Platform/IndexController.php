ff4eec879e7376dcf51458e862af6be8d018a51d
Harkály Gergő <git@harkalygergo.hu>
2023-12-19 17:54:47 +0100

    v2023.12.19.10 - IndexController created with sample backend

---
+++ src/Controller/Platform/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Platform;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('platform/backend/index.html.twig', [
+        ]);
+    }
+}
