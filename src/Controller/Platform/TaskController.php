64fd1e3f6848ef944dfc4084255b8b7cf8db5286
Harkály Gergő <git@harkalygergo.hu>
2023-12-27 11:25:50 +0100

    v2023.12.27.2 - task manager

---
+++ src/Controller/Platform/TaskController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller\Platform;
+
+use App\Entity\Task;
+use App\Repository\Platform\TaskRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TaskController extends AbstractController
+{
+    public function __construct(private ManagerRegistry $doctrine) {}
+
+    #[Route('/task', name: 'app_task')]
+    public function index(Request $request, TaskRepository $repository): Response
+    {
+        $tasks = $repository->findAll();
+
+        // creates a task object and initializes some data for this example
+        $entity = new Task();
+
+        $form = $this->createFormBuilder($entity)
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Frissítés',
+                'attr' => [
+                    'class' => 'my-1 btn btn-lg btn-success'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $em = $this->doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $data['notification'] = $user->getTitle(). ' sikeresen létrehozva.';
+        }
+
+        $i = 0;
+        $a = '<table class="table table-striped"><thead><tr><th>#</th><th>Cím</th><th>Tartalom</th></tr></thead><tbody>';
+        foreach ($tasks as $task) {
+            $a .= '<tr><td>'.++$i.'.</td><td>'.$task->getTitle().'</td><td>'.$task->getDescription().'</td></tr>';
+        }
+        $a .= '</tbody></table>';
+
+        $data = [
+            'title' => '<i class="bi bi-list-task"></i> Feladatkezelő',
+            'content' => $a,
+            'form' => $form
+        ];
+
+        return $this->render('platform/backend/v1/content.html.twig', $data);
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\Platform\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setID(int $ID): static
+    {
+        $this->ID = $ID;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
