24ba50a8edc9825eb4c6a67e4eb911b7ae1c0db6
Harkály Gergő <git@harkalygergo.hu>
2023-12-31 15:48:31 +0100

    v2023.12.31.1 - translations

---
+++ src/Controller/Platform/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Platform;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class LoginController extends AbstractController
+{
+    #[Route('/', name: 'admin_login')]
+    public function login(TranslatorInterface $translator): Response
+    {
+        $data = [
+            'title' => '<i class="bi bi-login"></i> Bejelentkezés<hr>',
+            'content' => ''];
+
+        return $this->render('platform/backend/login.html.twig', $data);
+    }
+}
