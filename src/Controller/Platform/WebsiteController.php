ff46f43024d71ee321c32b2c7a326da745ae8d1b
Harkály Gergő <git@harkalygergo.hu>
2024-01-07 22:52:31 +0100

    v2024.01.07.3 - website new and edit

--- src/Controller/Platform/WebsiteController.php
+++ src/Controller/Platform/WebsiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Platform;
 
+use App\Entity\Platform\Website;
 use App\Repository\Platform\WebsiteRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,8 +16,11 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class WebsiteController extends AbstractController
 {
-    public function __construct(private ManagerRegistry $doctrine)
+    private string $title = '';
+
+    public function __construct(private ManagerRegistry $doctrine, TranslatorInterface $translator)
     {
+        $this->title = '<i class="bi bi-globe"></i> '. $translator->trans('global.website');
     }
 
     #[Route('/{_locale}/admin/website/', name: 'admin_website')]
@@ -25,10 +29,94 @@ class WebsiteController extends AbstractController
         $dataList = $repository->findAll();
 
         $data = [
-            'title' => '<i class="bi bi-list-task"></i> Honlap',
+            'title' => $this->title,
             'dataList' => $dataList
         ];
 
         return $this->render('platform/backend/v1/list.html.twig', $data);
     }
+
+    #[Route('/{_locale}/admin/website/new/', name: 'admin_task_new')]
+    public function new(Request $request, TranslatorInterface $translator)
+    {
+        $entity = new Website();
+
+        $form = $this->createFormBuilder($entity)
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => $translator->trans('global.save'),
+                'attr' => [
+                    'class' => 'my-1 btn btn-lg btn-success'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Website $new */
+            $new = $form->getData();
+            $new->setCreatedAt(new \DateTimeImmutable('now')); // setting current date and time
+
+            $em = $this->doctrine->getManager();
+            $em->persist($new);
+            $em->flush();
+
+            $data['notification'] = $new->getTitle() . ' sikeresen létrehozva.';
+        }
+
+        $data = [
+            'title' => $this->title.'<hr>',
+            'form' => $form
+        ];
+
+        return $this->render('platform/backend/v1/form.html.twig', $data);
+    }
+
+    #[Route('/{_locale}/admin/website/edit/{id<\d+>}/', name: 'admin_task_edit')]
+    public function edit(Request $request, Website $entity)
+    {
+        $form = $this->createFormBuilder($entity)
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Frissítés',
+                'attr' => [
+                    'class' => 'my-1 btn btn-lg btn-success'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $em = $this->doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $data['notification'] = $user->getTitle() . ' sikeresen létrehozva.';
+        }
+
+        $data = [
+            'title' => $this->title.'<hr>',
+            'form' => $form
+        ];
+
+        return $this->render('platform/backend/v1/form.html.twig', $data);    }
 }
