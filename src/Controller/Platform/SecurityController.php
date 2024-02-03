b62b428a4edc7b7e5b186dd21a1c4ba66599ae0a
Harkály Gergő <git@harkalygergo.hu>
2024-02-03 14:53:36 +0100

    v2024.02.03.2 - new login logout security controller

---
+++ src/Controller/Platform/SecurityController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller\Platform;
+
+use App\Repository\Platform\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/{_locale?}/', name: 'admin_login')]
+    public function login(Request $request, Security $security, TranslatorInterface $translator, UserRepository $userRepository): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-lg bg-dark text-white'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                    'class' => 'form-control form-control-lg bg-dark text-white'
+                ]
+            ])
+            ->add('language', ChoiceType::class, [
+                'choices'  => [
+                    'english' => 'en',
+                    'magyar' => 'hu',
+                ],
+                'attr' => [
+                    'class' => 'form-control form-control-lg bg-dark text-white'
+                ]
+            ])
+            ->add('keep_logged_in', CheckboxType::class, [
+                'label'    => 'Keep me logged in',
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-check-input bg-dark text-white'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => $translator->trans('global.login'),
+                'attr' => [
+                    'class' => 'btn btn-outline-light btn-lg px-5 m-5'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            if ($this->isLoginCredentialsValid($userRepository, $security, $data['username'], $data['password']) ) {
+                return $this->redirectToRoute('admin_index');
+            }
+        }
+
+        $data = [
+            'title' => '<i class="bi bi-login"></i> '.$translator->trans('global.login').'<hr>',
+            'content' => '',
+            'form' => $form->createView(),
+        ];
+
+        return $this->render('platform/backend/login.html.twig', $data);
+    }
+
+    public function isLoginCredentialsValid(UserRepository $userRepository, Security $security, string $username, string $password): bool
+    {
+        $findUser = $userRepository->findBy([
+            'username'  => $username,
+            'password'  => $password,
+        ]);
+
+        if ($findUser) {
+            $security->login($findUser[0], 'form_login', 'main');
+            return true;
+        }
+
+        return false;
+    }
+
+    #[Route('/{_locale?}/logout/', name: 'admin_logout')]
+    public function logout(Security $security)
+    {
+        if ($security->getUser()) {
+            $security->logout(false);
+        }
+
+        return $this->redirectToRoute('admin_login');
+    }
+}
--- src/Entity/Platform/User.php
+++ src/Entity/Platform/User.php
@@ -3,11 +3,20 @@
 namespace App\Entity\Platform;
 
 use App\Repository\Platform\UserRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    // We can use constants for roles to find usages all over the application rather
+    // than doing a full-text search on the "ROLE_" string.
+    // It also prevents from making typo errors.
+    final public const ROLE_USER = 'ROLE_USER';
+    final public const ROLE_ADMIN = 'ROLE_ADMIN';
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -24,6 +33,12 @@ class User
 
     #[ORM\Column(length: 8, nullable: true)]
     private ?string $language = null;
+
+    /**
+     * @var string[]
+     */
+    #[ORM\Column(type: Types::JSON, options: ["default" => self::ROLE_USER])]
+    private array $roles = [];
 
     public function getId(): ?int
     {
@@ -74,5 +89,28 @@ class User
     public function setLanguage(?string $language): void
     {
         $this->language = $language;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+
+        // guarantees that a user always has at least one role for security
+        if (empty($roles)) {
+            $roles[] = self::ROLE_USER;
+        }
+
+        return array_unique($roles);
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
     }
 }
