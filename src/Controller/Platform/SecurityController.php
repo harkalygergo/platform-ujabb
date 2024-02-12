8ec7ce39e926a12855757a573fa249a84eb8a1f8
Harkály Gergő <git@harkalygergo.hu>
2024-02-12 17:48:39 +0100

    v2024.02.12.1 - remove unnecessary granted row

--- src/Controller/Platform/SecurityController.php
+++ src/Controller/Platform/SecurityController.php
@@ -17,10 +17,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-#[IsGranted(User::ROLE_USER)]
 class SecurityController extends AbstractController
 {
     public function __construct(
