bf4e7985dd7148a5e6cbf7cf9794984de2144834
Harkály Gergő <git@harkalygergo.hu>
2024-05-03 16:48:01 +0200

    v2024.05.03.3 - added inactive drive sidebar menu

--- src/Controller/Platform/_PlatformAbstractController.php
+++ src/Controller/Platform/_PlatformAbstractController.php
@@ -160,6 +160,7 @@ class _PlatformAbstractController extends AbstractController
             [
                 'title' => 'Eszközök',
                 'elements' => [
+                    '#drive' => '<i class="bi bi-hdd"></i> Tárhely',
                     '#clients' => '<i class="bi bi-link-45deg"></i> Link rövidítés',
                     '#exportimport' => '<i class="bi bi-arrow-down-up"></i> Export/import',
                 ]
