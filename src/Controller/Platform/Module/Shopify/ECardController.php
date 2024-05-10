5eb85779e28c2c4de80e9ba1a07b823ae852d408
Harkály Gergő <git@harkalygergo.hu>
2024-05-10 17:55:59 +0200

    v2024.05.10.8 - save webhook content to database

--- src/Controller/Platform/Module/Shopify/ECardController.php
+++ src/Controller/Platform/Module/Shopify/ECardController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller\Platform\Module\Shopify;
 
+use App\Entity\Platform\Module\Shopify\ECard;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class ECardController
 {
+    #[Required]
+    private ManagerRegistry $doctrine;
+
     #[Route('/shopify/ecard/order', name: 'shopify_ecard_webhook')]
-    public function webhook(Request $request): JsonResponse
+    public function webhook(): JsonResponse
     {
+        // Get the raw POST data sent by Shopify
+        $webhookContent = file_get_contents('php://input');
+
+        // Save the order JSON to the database
+        $eCard = new ECard();
+        $eCard->setOrderJSON($webhookContent);
+        $em = $this->doctrine->getManager();
+        $em->persist($eCard);
+        $em->flush();
+
         return new JsonResponse('Order Webhook');
     }
 
