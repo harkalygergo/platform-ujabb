d99a855c6a4dc760023246fa8ec4816ee343c6ba
Harkály Gergő <git@harkalygergo.hu>
2024-05-10 16:54:18 +0200

    v2024.05.10.5 - rename Shopify eCard webhook controller

--- src/Controller/Platform/Module/Shopify/EcardController.php
+++ src/Controller/Platform/Module/Shopify/EcardController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
-class Ecard
+class EcardController
 {
     #[Route('/shopify/ecard/order', name: 'shopify_ecard_webhook')]
     public function webhook(Request $request): JsonResponse
