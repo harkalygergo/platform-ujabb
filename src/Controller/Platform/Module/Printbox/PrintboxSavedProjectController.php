09945259585a3197ff80fe060838105e1c364065
Harkály Gergő <git@harkalygergo.hu>
2024-05-24 14:32:33 +0200

    v2024.05.24.2 - modify because of CORS problem

--- src/Controller/Platform/Module/Printbox/PrintboxSavedProjectController.php
+++ src/Controller/Platform/Module/Printbox/PrintboxSavedProjectController.php
@@ -181,6 +181,9 @@ class PrintboxSavedProjectController extends _PlatformAbstractController
 
         $response = new JsonResponse($json);
         $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Referrer-Policy', 'no-referrer');
+        $response->headers->set('Cross-Origin-Opener-Policy', 'same-origin');
+        $response->headers->set('Cross-Origin-Embedder-Policy', 'require-corp');
 
         return $response;
     }
