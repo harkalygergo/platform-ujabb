2567f13564a40c31b6690bdf5cdd5fe31bce064d
Harkály Gergő <git@harkalygergo.hu>
2024-05-29 12:59:37 +0200

    v2024.05.29.2 - make favourites nullable

--- src/Entity/Platform/User.php
+++ src/Entity/Platform/User.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -74,7 +75,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $profileImageUrl = null;
 
     // add favourites as collection of route names
-    #[ORM\Column(type: Types::JSON)]
+    #[ORM\Column(type: Types::JSON, nullable: true, options: ["default" => null])]
     private ?array $favourites = null;
 
     public function __construct()
